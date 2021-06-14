ae35dbc5eebfed62ced80235d5e4243838767781
Roman <romanvasylyna888@gmail.com>
2021-06-14 17:45:38 +0300

    Started

--- src/index.js
+++ src/index.js
@@ -3,6 +3,50 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+// Redux parts :
+// 1. Store - global state that can be accessed from everywhere in the app
+
+// 2. Action - defines the action to be made
+// it checks which action is called and store will be modified accordingly
+
+// Increase number of pets
+const petIncrement = () => {
+  return {
+    type: "PET_INCREASED"
+  }
+}
+
+const petDecrement = () => {
+  return {
+    type: "PET_DECREASED"
+  }
+}
+
+// 3. Reducer - condition that changes an action from one state to another
+const petCounter = (state=0, action) => {
+  switch (action.type) {
+    case "PET_INCREASED":
+      return state + 1;
+    case "PET_DECREASED":
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+
+let store = createStore(petCounter); // Create a store with reducer as a param
+store.subscribe(() => console.log(store.getState()));
+
+
+// 4. Dispatch - changes store via reducer
+store.dispatch(petIncrement());
+store.dispatch(petIncrement());
+store.dispatch(petDecrement());
+
+
 
 ReactDOM.render(
   <React.StrictMode>
